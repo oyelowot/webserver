4ec25e8532934e2e4bdf5d574870a2c9923f73bc
EC2 Default User <ec2-user@ip-172-31-51-238.ec2.internal>
2016-09-27 22:00:24 +0000

    initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,12 @@
-#
+package "httpd"
+
+service "httpd" do
+  action [:start,:enable]
+end
+
+file '/var/www/html/index.html' do
+  content 'Hello World!'
+end
 # Cookbook Name:: webserver
 # Recipe:: default
 #
